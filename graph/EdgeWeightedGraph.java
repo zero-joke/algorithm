5f054f8c5452fead27728638e63a01c1a1c7933c
jibu <jibu@pinduoduo.com>
2020-07-16 21:13:02 +0800

    最小生成树

---
+++ graph/EdgeWeightedGraph.java
@@ -0,0 +1,39 @@
+package graph;
+
+import base.Bag;
+
+/**
+ * 加权无向图的数据类型
+ */
+public class EdgeWeightedGraph {
+    private final int V; // 顶点总数
+    private int E; // 边数
+    private Bag<Edge> adj[]; // 邻接表
+
+    public EdgeWeightedGraph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Edge>[]) new Bag[V];
+        for (int i = 0; i < V; i++)
+            adj[i] = new Bag<Edge>();
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public void addEdge(Edge e) {
+        int v = e.either();
+        adj[v].add(e);
+        adj[e.other(v)].add(e);
+        E++;
+    }
+
+    public Iterable<Edge> adj(int v) {
+        return adj[v];
+    }
+}
---
+++ graph/LazyPrimMST.java
@@ -0,0 +1,43 @@
+package graph;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * 最小生成树的 Prim 算法的延时实现
+ */
+public class LazyPrimMST {
+    private boolean marked[]; // 已标记顶点
+    private Queue<Edge> mst; // 最小生成树的边
+    private PriorityQueue<Edge> pq; // 横切边（包括失效的边）
+
+    public LazyPrimMST(EdgeWeightedGraph G) {
+        pq = new PriorityQueue<>();
+        marked = new boolean[G.V()];
+        mst = new LinkedList<>();
+
+        visit(G, 0);
+        while (!pq.isEmpty()) {
+            Edge e = pq.poll(); // 从pq中得到权重最小的边
+            int v = e.either(), w = e.other(v);
+            if (marked[v] && marked[w]) // 跳过失效的边
+                continue;
+            mst.offer(e); // 将权重最小的边添加到树中
+
+            // 继续访问边的未标记顶点
+            if (!marked[v])
+                visit(G, v);
+            if (!marked[w])
+                visit(G, w);
+        }
+    }
+
+    private void visit(EdgeWeightedGraph G, int v) {
+        // 标记顶点v并将所有连接v和未被标记顶点的边加入pq
+        marked[v] = true;
+        for (Edge e : G.adj(v))
+            if (!marked[e.other(v)])
+                pq.offer(e);
+    }
+}
